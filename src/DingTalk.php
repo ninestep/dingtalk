9b4118017ddf136bdea372c8ca9e085cb43de56f
杨兵 <859696354@qq.com>
2021-08-20 10:27:54 +0800

    增加其他回调

--- src/DingTalk.php
+++ src/DingTalk.php
@@ -30,19 +30,73 @@ class DingTalk
      */
     public function __construct($config = [])
     {
-        if (empty($config)){
+        if (empty($config)) {
             $config = Cache::get('config');
-        }else{
-            Cache::set('config',$config);
+        } else {
+            Cache::set('config', $config);
         }
-        if (empty($config)){
+        if (empty($config)) {
             throw new DingTalkException('配置信息错误');
         }
         self::$config = $config;
     }
 
+    /**
+     * 部门
+     * @return Dept
+     */
     public function dept()
     {
         return new Dept();
+    }
+
+    /**
+     * 用户
+     * @return User
+     */
+    public function user()
+    {
+        return new User();
+    }
+
+    /**
+     * 角色
+     * @return Role
+     */
+    public function role()
+    {
+        return new Role();
+    }
+
+    /**
+     * js_api
+     * @return JsApi
+     */
+    public function js_api()
+    {
+        return new JsApi();
+    }
+
+    /**
+     * 消息发送
+     * @return Message
+     */
+    public function message()
+    {
+        return new Message();
+    }
+
+    /**
+     * 回调
+     * @param string $token 钉钉开放平台上，开发者设置的token
+     * @param string $aesKey 钉钉开放台上，开发者设置的EncodingAESKey
+     * @param string $ownerKey 企业自建应用-事件订阅, 使用appKey
+     *                       企业自建应用-注册回调地址, 使用corpId
+     *                       第三方企业应用, 使用suiteKey
+     * @return CallBack
+     */
+    public function callback(string $token, string $aesKey, string $ownerKey)
+    {
+        return new CallBack($token, $aesKey, $ownerKey);
     }
 }
